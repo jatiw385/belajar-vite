756aee727d728397a5c932bd78d6e54f6e2ee3fc
jatiw385 <jatiwicaksono12@gmail.com>
2025-09-15 18:57:19 +0700

    first

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Home from './components/Home.vue';
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+    <Navbar/>
+    <Home/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="flex justify-between">
+        <div class="flex justify-center items-center">
+            <img src="../assets/menu.svg" alt="">
+            <img src="../assets/bur.svg" alt="">
+        </div>
+        <div>
+            <div>
+                <span class="material-symbols-outlined">favorite</span>
+                <span class="material-symbols-outlined">language</span>
+                <span class="material-symbols-outlined">shopping_cart</span>
+            </div>
+            <div class="flex gap-1.5">
+                <a href="">Home</a>
+                <a href="">Bantuan</a>
+                <a href="">Search</a>
+            </div>
+        </div>
+    </nav>
+</template>
